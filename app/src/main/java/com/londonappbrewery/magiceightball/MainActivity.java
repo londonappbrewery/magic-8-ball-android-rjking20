bff16fb620937984e77f08214fb6f47f6d409d3a
Rj King <42872016+rjking20@users.noreply.github.com>
2020-01-12 12:49:40 +0530

    Add files via upload

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,13 @@
-package com.londonappbrewery.magiceightball;
+package com.rjfication.magicball;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button aSk=findViewById(R.id.ask);
+        final ImageView Magic=findViewById(R.id.magic);
+
+        //-----------Using Array for storing all images
+
+        final int [] ball={R.drawable.good,
+                     R.drawable.nah,
+                     R.drawable.yeah,
+                     R.drawable.go
+        };
+
+
+
+
+
+        aSk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                //-----------Using Random class for pick images randomly
+
+                Random random=new Random();
+
+                final int position=random.nextInt(4);
+
+                Magic.setImageResource(ball[position]);
+            }
+        });
     }
 }
